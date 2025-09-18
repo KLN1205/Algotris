83e383884f3360eb17c746ece404e6a4bef20ee6
ValentinTRCR <valentin.trcr@eduge.ch>
2025-09-18 15:00:19 +0200

    ajout du code pour la classe pièce pour l'instant y'a juste la génération de la pièce

--- js/piece.js
+++ js/piece.js
@@ -1,8 +1,46 @@
-class Piece 
-{
+class Piece {
     form;
     color;
     position;
+    image;
 
+    constructor(form, color, position) {
+        this.form = form;
+        this.color = color;
+        this.position = position;
 
-} 
+        
+        this.image = this.GenererForme();
+    }
+
+    GenererForme() {
+        const rows = this.form.length;
+        const cols = this.form[0].length;
+        const tileSize = 30; 
+
+        const canvas = document.createElement('canvas');
+        canvas.width = cols * tileSize;
+        canvas.height = rows * tileSize;
+        const ctx = canvas.getContext('2d');
+
+  
+        for (let y = 0; y < rows; y++) {
+            for (let x = 0; x < cols; x++) {
+                if (this.form[y][x] === 1) {
+                    ctx.fillStyle = this.color;
+                    ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+                    ctx.strokeStyle = "black"; 
+                    ctx.strokeRect(x * tileSize, y * tileSize, tileSize, tileSize);
+                }
+            }
+        }
+
+        const img = new Image();
+        img.src = canvas.toDataURL();
+        return img;
+    }
+
+    Descendre() {
+        this.position.y += 1;
+    }
+}
